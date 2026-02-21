122fce5d059f44f1ce9881e944df99af1dacb301
samuelmamootil <mamootilsamuel1@gmail.com>
2026-02-20 18:12:22 -0600

    delet powershell file

--- test-textract.ps1
+++
@@ -1,4 +0,0 @@
-$pdfPath = "c:\Sam\2026\ReRhythm\ReRhythm.Web\Resume-Samuel-Mamootil-NVIDIA.pdf"
-$bytes = [System.IO.File]::ReadAllBytes($pdfPath)
-Write-Host "PDF Size: $($bytes.Length) bytes"
-Write-Host "First 10 bytes: $($bytes[0..9] -join ', ')"
